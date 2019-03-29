6f5611ffb7aadfb14da65b13e221ee1820ce86b4
Siam Abd Al-Ilah <siam.abdal-ilah@wustl.edu>
2019-03-29 18:05:04 -0500

    changed something

--- app/src/main/java/com/example/groop/MainActivity.kt
+++ app/src/main/java/com/example/groop/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        android.util.Log.d("change", "Change")
     }
 }
