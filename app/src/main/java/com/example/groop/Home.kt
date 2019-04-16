7335c83ef0185c6f026f33811096df6273b49238
Reid parisi <>
2019-04-16 15:03:15 -0500

    commented out home

--- app/src/main/java/com/example/groop/Home.kt
+++ app/src/main/java/com/example/groop/Home.kt
@@ -2,12 +2,12 @@ package com.example.groop
 
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import android.arch.lifecycle.Observer
+//import android.arch.lifecycle.Observer
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.util.DiffUtil
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+//import android.support.v7.app.AppCompatActivity
+//import android.support.v7.util.DiffUtil
+//import android.support.v7.widget.LinearLayoutManager
+//import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
