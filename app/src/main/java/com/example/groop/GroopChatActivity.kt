7a729ff7e83575f35188061aa88ed26b556b62ea
romancaccardi <35588729+romancaccardi@users.noreply.github.com>
2019-04-19 02:16:47 -0500

    added a bunch of copy-pasted stuff

--- app/src/main/java/com/example/groop/GroopChatActivity.kt
+++ app/src/main/java/com/example/groop/GroopChatActivity.kt
@@ -2,11 +2,19 @@ package com.example.groop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.groop.Util.Groop
 
 class GroopChatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_groop_chat)
+
+        //should have passed in a Groop object when entering this activity,
+        // so let's get that li'l guy
+        //key should be "groop"
+        val groop = intent.getSerializableExtra("groop") as Groop
+
+        //okay, first off, we've got to connect to the thing
     }
 }
