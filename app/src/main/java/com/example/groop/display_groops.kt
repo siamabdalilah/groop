91a6eb8d12974a5cc2e26cce0ad103a19aaafac9
Reid parisi <>
2019-04-20 02:26:06 -0500

    not working but oh well

--- app/src/main/java/com/example/groop/display_groops.kt
+++ app/src/main/java/com/example/groop/display_groops.kt
@@ -58,11 +58,12 @@ val user = User("telemonian@gmail.com", "Billiamson McGee", GeoPoint(1.1, 0.0), 
                             }
                         }
                     }
+                    else{
+                        my_groops=activity_list_temp
+                    }
                     adapter.notifyDataSetChanged()
                 }
-                else{
-                    my_groops=activity_list_temp
-                }
+
             }
         search_by_distance.visibility=View.GONE
         search_by_distance.setOnFocusChangeListener { v, hasFocus ->
